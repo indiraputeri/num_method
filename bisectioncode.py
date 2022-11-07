834d827ceea30756d57935408c5fb73db82b99cb
indiraputeri <indiraputeri@uinmataram.ac.id>
2022-11-07 23:17:15 +0800

    Estimation Method
    first commit

--- bisectioncode.py
+++ bisectioncode.py
@@ -1,10 +1,10 @@
 #Program Phyton di PyCharm untuk mendapatkan akar persamaan x-cos(x)
 #dengan menggunakan metode bagi dua
 #dengan batas (0.6,0.8) dan toleransi error 10e-14
-#program ini diadaptasi dari Andrew Dotson.
+#program ini diadaptasi dari Andrew Dotson
 
 from typing import Any, Union
-import numpy as np
+import NumPy as np
 from matplotlib.pyplot import plot, grid, show
 
 def f(x):
